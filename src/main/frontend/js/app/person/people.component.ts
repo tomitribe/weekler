2981ae1d5199da5c0e059a278bfcafc35368f3b9
Romain Manni-Bucau <rmannibucau@gmail.com>
2016-01-18 10:23:34 +0100

    adding pagination links for people

--- src/main/frontend/js/app/person/people.component.ts
+++ src/main/frontend/js/app/person/people.component.ts
@@ -1,5 +1,6 @@
 import {Component, View} from "angular2/core";
 import {RouterLink} from "angular2/router";
+import {Observable} from "rxjs/Observable";
 import {Page} from "../util/page";
 import {Person} from "./person";
 import {PersonService} from "./person.service";
@@ -10,9 +11,23 @@ import {PersonService} from "./person.service";
     templateUrl: 'partial/person/people.html'
 })
 export class PeopleComponent {
+    pageNumber: number = -1;
     page: Page<Person> = new Page<Person>(0, false, []);
 
     constructor(private personService: PersonService) {
-        personService.findAll(0).subscribe(page => this.page = page);
+        this.loadNext();
+    }
+
+    loadPrevious() {
+        if (this.pageNumber <= 0) {
+            return;
+        }
+        this.pageNumber--;
+        this.personService.findAll(this.pageNumber).subscribe(page => this.page = page);
+    }
+
+    loadNext() {
+        this.pageNumber++;
+        this.personService.findAll(this.pageNumber).subscribe(page => this.page = page);
     }
 }
